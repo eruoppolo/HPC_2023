9a8a76863a28263700dc15d81482850f7190396a
Emanuele Ruoppolo <147620506+eruoppolo@users.noreply.github.com>
2024-11-11 12:42:32 +0100

    Update bcast0_mapping_by_core.sh

--- ex1/scripts/bcast0_mapping_by_core.sh
+++ ex1/scripts/bcast0_mapping_by_core.sh
@@ -12,24 +12,25 @@ echo "Nodi utilizzati: $SLURM_NODELIST"
 echo "BROADCAST 0"
 echo "Processes,Size,Latency" > bcast0_core_mapping_epyc.csv
 
-# Numero di ripetizioni per ottenere una media
+# Number of repetitions to get an average result
 repetitions=10000
 
-# Ciclo esterno per il numero di processori
+# Cycle over processors
 for processes_size in {1..8}
 do
+    # Set number of processors from 2^1 to 2^8
     processes=$((2**processes_size))
-    # Ciclo interno per la dimensione del messaggio da 2^1 a 2^20
+    # Set message size from 2^1 to 2^18
     for size_power in {1..18}
     do
-        # Calcola la dimensione come 2 elevato alla potenza corrente
+        # Compute message size
         size=$((2**size_power))
 
-        # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
+        # Perform osu_bcast
         result_bcast=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_bcast_algorithm 0 ../osu_bcast -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
 
         echo "$processes, $size, $result_bcast"
-        # Scrivi i risultati nel file CSV
+        # Write results on CSV
         echo "$processes,$size,$result_bcast" >> bcast0_core_mapping_epyc.csv
     done
 done
