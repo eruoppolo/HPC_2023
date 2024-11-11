b078fa529a2d3525b07b90f39e6db3c059184a40
Emanuele Ruoppolo <147620506+eruoppolo@users.noreply.github.com>
2024-11-11 13:02:36 +0100

    Update scatter0_fixed_core_epyc.sh

--- ex1/scripts/scatter0_fixed_core_epyc.sh
+++ ex1/scripts/scatter0_fixed_core_epyc.sh
@@ -14,65 +14,26 @@ echo "Processes,Size,Latency" > scatter1_fixed_core.csv
 echo "Processes,Size,Latency" > scatter2_fixed_core.csv
 echo "Processes,Size,Latency" > scatter3_fixed_core.csv
 
-# Numero di ripetizioni per ottenere una media
+# Repetitions to get an average result
 repetitions=10000
+
+# Fixed message size
 size=4
 
-# Ciclo esterno per il numero di processori
-for processes in {2..256}
+# Cycling over different algorithms
+for algoritm in {0..3}
 do
-    # Calcola la dimensione come 2 elevato alla potenza corrente
-
-    # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
-    result_scatter=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_scatter_algorithm 0 ../osu_scatter -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
-
-    echo "$processes, $size, $result_scatter"
-    # Scrivi i risultati nel file CSV
-    echo "$processes,$size,$result_scatter" >> scatter0_fixed_core.csv
-
-done
-
-# Ciclo esterno per il numero di processori
-for processes in {2..256}
-do
-    # Calcola la dimensione come 2 elevato alla potenza corrente
-
-    # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
-    result_scatter=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_scatter_algorithm 1 ../osu_scatter -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
-
-    echo "$processes, $size, $result_scatter"
-    # Scrivi i risultati nel file CSV
-    echo "$processes,$size,$result_scatter" >> scatter1_fixed_core.csv
-
-done
-
-# Ciclo esterno per il numero di processori
-for processes in {2..256}
-do
-    # Calcola la dimensione come 2 elevato alla potenza corrente
-
-    # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
-    result_scatter=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_scatter_algorithm 2 ../osu_scatter -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
-
-    echo "$processes, $size, $result_scatter"
-    # Scrivi i risultati nel file CSV
-    echo "$processes,$size,$result_scatter" >> scatter2_fixed_core.csv
-
-done
-
-
-# Ciclo esterno per il numero di processori
-for processes in {2..256}
-do
-    # Calcola la dimensione come 2 elevato alla potenza corrente
-
-    # Esegui osu_bcast con numero di processi, dimensione fissa e numero di ripetizioni su due nodi
-    result_scatter=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_scatter_algorithm 3 ../osu_scatter -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
-
-    echo "$processes, $size, $result_scatter"
-    # Scrivi i risultati nel file CSV
-    echo "$processes,$size,$result_scatter" >> scatter3_fixed_core.csv
-
+    for processes in {2..256}
+    do
+    
+        # Perform osu_scatter with current processors, fixed message size and fixed number of repetitions
+        result_scatter=$(mpirun --map-by core -np $processes --mca coll_tuned_use_dynamic_rules true --mca coll_tuned_scatter_algorithm $algoritm ../osu_scatter -m $size -x $repetitions -i $repetitions | tail -n 1 | awk '{print $2}')
+    
+        echo "$processes, $size, $result_scatter"
+        # Write results on CSV
+        echo "$processes,$size,$result_scatter" >> scatter0_fixed_core.csv
+    
+    done
 done
 
 
